3a5bda923217081e62be465a40d19c0738ca0777
gmunster <gmunster100@gmail.com>
2025-02-07 23:34:10 -0600

    First commit

---
+++ main.py
@@ -0,0 +1,28 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+from constants import *
+
+
+def main():
+  print("Starting asteroids!")
+  print(f"Screen width: {SCREEN_WIDTH}")
+  print(f"Screen height: {SCREEN_HEIGHT}")
+
+  screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+  running = True
+  while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill("black")
+        pygame.display.flip()
+
+  pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
