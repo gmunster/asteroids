ff10c3bc470e08de7deeba839fdba5ac9145a9fb
gmunster <gmunster100@gmail.com>
2025-02-15 09:28:38 -0600

    Final Asteroid Game working

--- main.py
+++ main.py
@@ -2,29 +2,67 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+import sys
 from constants import *
-
-clock = pygame.time.Clock()
-dt = 0
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot  
 
 
 
 def main():
+  pygame.init()
+  screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+  clock = pygame.time.Clock()
+  dt = 0
+
+  updatable = pygame.sprite.Group()
+  drawable = pygame.sprite.Group()
+  asteroids = pygame.sprite.Group()
+  shots = pygame.sprite.Group()
+
+  Asteroid.containers = (asteroids, updatable, drawable)
+  AsteroidField.containers = (updatable)
+  Shot.containers = (shots, updatable, drawable)
+
+  asteroid_field = AsteroidField()
+
+  player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+  updatable.add(player)
+  drawable.add(player)
   print("Starting asteroids!")
   print(f"Screen width: {SCREEN_WIDTH}")
   print(f"Screen height: {SCREEN_HEIGHT}")
-
-  screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-  running = True
-  while running:
+  
+  while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
+                return
+        updatable.update(dt)
 
+        for asteroid in asteroids:
+            for shot in shots.copy():  # Iterate over a copy to safely remove items
+                if shot.collides_with(asteroid):
+                   shot.kill()
+                   asteroid.split() 
+
+            if player.collides_with(asteroid):
+                print("Game Over!")
+                pygame.quit()
+                sys.exit()
+                
+
+        
         screen.fill("black")
+        for entity in drawable:
+            entity.draw(screen)
+        player.draw(screen)
         pygame.display.flip()
+        
+
         dt = clock.tick(60) / 1000
+        
 
   pygame.quit()
 
